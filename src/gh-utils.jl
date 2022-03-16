4eb533946bb65b2ab4dd646dc8a8dfddbe623bb2
SimonDanisch <sdanisch@protonmail.com>
2022-03-16 20:55:25 +0100

    improve blogpost a bit

--- src/gh-utils.jl
+++ src/gh-utils.jl
@@ -29,10 +29,10 @@ function commits_between(tag1, tag2, auth; repo="JuliaPlots/Makie.jl")
 end
 
 function JSServe.jsrender(owner::GitHub.Owner)
-    name = DOM.span(string(owner.login), style="margin: 2px; font-size:20px;color: 'gray")
-    img = DOM.img(src=owner.avatar_url, style="border-radius: 50%", width=24)
-    img_name = DOM.div(img, name; style="display: flex; align-items: center; justify-content: center")
-    return DOM.div(
+    name = DOM.span(string(owner.login), style="margin: 2px; color: 'gray")
+    img = DOM.img(src=owner.avatar_url, style="border-radius: 50%", width=22)
+    img_name = DOM.div(img, name; style="display: flex")
+    return DOM.span(
         DOM.a(img_name, href=owner.html_url; style="color: gray; text-decoration: none");
         style="padding: 2px")
 end
