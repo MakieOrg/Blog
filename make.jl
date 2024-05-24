10e107f77eabc9f45b3beed6ee7198c44314c828
SimonDanisch <sdanisch@protonmail.com>
2024-05-24 14:19:05 +0200

    move images and add intro

--- make.jl
+++ make.jl
@@ -13,3 +13,18 @@ using CairoMakie
 Blog.make("v0.21")
 Blog.make("index")
 
+
+
+## Replace 
+md_path = joinpath(@__DIR__, "src", "pages", "blogposts", "v0.21.md")
+txt = read(md_path, String)
+
+matchi = Base.eachmatch(r"https://hackmd\.io/_uploads/(.*?)\.png", txt)
+
+using Downloads
+assets = joinpath(@__DIR__, "docs", "images")
+for match in matchi
+    Downloads.download(String(match.match), joinpath(assets, String(match[1]) * ".png"))
+end
+replace(txt, r"https://hackmd\.io/_uploads/(.*?)\.png" => "/images/$1.png")
+write(md_path, txt)
