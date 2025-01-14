957f30428e48237d2bf83e7b81ee52b170826bf2
SimonDanisch <sdanisch@protonmail.com>
2025-01-14 14:42:46 +0100

    fix build

--- docs/make.jl
+++ docs/make.jl
@@ -12,7 +12,7 @@ title = "Makie Blog"
 link = "https://blog.makie.org"
 description = "A Blog about anything new in the Makie world"
 
-function create_routes(f, page_folder)
+function create_routes(f, page_folder, destination)
     routes = Routes()
     folders = filter(isdir, readdir(page_folder; join=true))
     entries = map(folders) do dir
@@ -28,14 +28,11 @@ function create_routes(f, page_folder)
     end
     site_entries = map(x -> x[2], entries)
     routes["/"] = App(f(Bonito.Col(site_entries...)))
+    rss_path = joinpath(destination, "rss.xml")
+    BonitoSites.generate_rss_feed(site_entries, rss_path; title, link, description, relative_path="./website/")
     return routes
 end
 
-function make(routes, destination)
-    Bonito.export_static(destination, routes)
-    rss_path = joinpath(destination, "rss.xml")
-    BonitoSites.generate_rss_feed(site_entries, rss_path; title, link, description, relative_path="./website/")
-end
 
 ##
 # using Revise
@@ -46,9 +43,10 @@ end
 
 ##
 build = Blog.site_path("build")
-isdir(build) && rm(build; recursive=true)
-routes = create_routes(Blog.Page, Blog.markdown())
-make(Blog.Page, Blog.markdown(), build)
+isdir(build) && rm(build; recursive=true);
+!isdir(build) && mkdir(build)
+routes = create_routes(Blog.Page, Blog.markdown(), build);
+Bonito.export_static(build, routes);
 cp(Blog.assetpath("images"), Blog.site_path("build", "images"))
 
 
